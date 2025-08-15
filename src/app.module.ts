2fa3db8182e3ba40be27dfdbe9b62ec5ea068b96
MEETPATEL322 <meet1patel0@gmail.com>
2025-08-15 18:44:20 +0530

    nestjs setup and connection mysql

---
+++ src/app.module.ts
@@ -0,0 +1,41 @@
+import { Module, OnModuleInit } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // makes env variables available everywhere
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<'mysql'>('DB_TYPE'),
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        synchronize: true, // ❗ disable in production
+        // entities: [Tenant, User],
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements OnModuleInit {
+  constructor(private dataSource: DataSource) {}
+
+  async onModuleInit() {
+    if (this.dataSource.isInitialized) {
+      console.log('✅ MySQL Database connected successfully');
+    } else {
+      console.error('❌ Failed to connect to MySQL Database');
+    }
+  }
+}
